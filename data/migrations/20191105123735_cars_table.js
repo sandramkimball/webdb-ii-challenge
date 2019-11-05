9a46a06a5f42405721b3ca0847ef615d369c1e28
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-11-05 12:41:48 -0800

    initial commit

---
+++ data/migrations/20191105123735_cars_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', function(table){
+        table.increments();
+
+        table.string('vin', 128).notNullable();
+        table.string('make', 128).notNullable();
+        table.string('model', 128).notNullable();
+        table.integer('mileage', 128).notNullable();
+        table.string('transmission', 128);
+        table.string('titleStatus', 128);
+
+        table.timestamps(true, true);
+    });
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
